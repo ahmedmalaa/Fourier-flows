17a9fe91eae69276944ec338808ce9af2c679307
Evgeny Saveliev <e.s.saveliev@gmail.com>
2021-09-23 16:48:41 +0100

    Update to code from Ahmed as of 20210920

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Fourier-flows
+
+Code for [Generative Time-series Modeling with Fourier Flows](https://openreview.net/forum?id=PpshD0AXfA).
+
+Please cite:
+~~~bibtex
+@inproceedings{alaa2020generative,
+  title={Generative Time-series Modeling with Fourier Flows},
+  author={Alaa, Ahmed and Chan, Alex James and van der Schaar, Mihaela},
+  booktitle={International Conference on Learning Representations},
+  year={2020}
+}
+~~~
--- data_loading.py
+++ data_loading.py
@@ -25,66 +25,61 @@ import numpy as np
 import pickle
 
 
-def sine_data_generation (no, seq_len, dim):
-    """Sine data generation.
+def MinMaxScaler(data):
+  """Min Max normalizer.
   
-      Args:
+  Args:
+    - data: original data
+  
+  Returns:
+    - norm_data: normalized data
+  """
+  numerator = data - np.min(data, 0)
+  denominator = np.max(data, 0) - np.min(data, 0)
+  norm_data = numerator / (denominator + 1e-7)
+  return norm_data
+
+
+def sine_data_generation (no, seq_len, dim):
+  """Sine data generation.
+  
+  Args:
     - no: the number of samples
     - seq_len: sequence length of the time-series
     - dim: feature dimensions
     
-    Returns:
+  Returns:
     - data: generated data
-    """  
-    # Initialize the output
-    data = list()
+  """  
+  # Initialize the output
+  data = list()
 
-    # Generate sine data
-    for i in range(no):      
-        
-        # Initialize each time-series
-        temp = list()
-    
-        # For each feature
-        for k in range(dim):
-            
-            # Randomly drawn frequency and phase
-            freq = np.random.uniform(0, 0.1)            
-            phase = np.random.uniform(0, 0.1)
+  # Generate sine data
+  for i in range(no):      
+    # Initialize each time-series
+    temp = list()
+    # For each feature
+    for k in range(dim):
+      # Randomly drawn frequency and phase
+      freq = np.random.uniform(0, 0.1)            
+      phase = np.random.uniform(0, 0.1)
 
-            freq      = np.random.beta(2, 2) #np.random.uniform(0, 0.1)            
-            phase     = np.random.normal() 
+      freq      = np.random.beta(2, 2) #np.random.uniform(0, 0.1)            
+      phase     = np.random.normal() 
           
-            # Generate sine signal based on the drawn frequency and phase
-            temp_data = [np.sin(freq * j + phase) for j in range(seq_len)] 
-            temp.append(temp_data)
+      # Generate sine signal based on the drawn frequency and phase
+      temp_data = [np.sin(freq * j + phase) for j in range(seq_len)] 
+      temp.append(temp_data)
         
-            # Align row/column
-            temp = np.transpose(np.asarray(temp))        
-            # Normalize to [0,1]
-            temp = (temp + 1)*0.5
-            # Stack the generated data
-            data.append(temp)
+    # Align row/column
+    temp = np.transpose(np.asarray(temp))        
+    # Normalize to [0,1]
+    temp = (temp + 1)*0.5
+    # Stack the generated data
+    data.append(temp)
                 
-    return data
+  return data
     
-
-def MinMaxScaler(data):
-    """Min Max normalizer.
-  
-    Args:
-    - data: original data
-  
-    Returns:
-    - norm_data: normalized data
-    """
-  
-    numerator = data - np.min(data, 0)
-    denominator = np.max(data, 0) - np.min(data, 0)
-    norm_data = numerator / (denominator + 1e-7)
-    
-    return norm_data
-
 
 def real_data_loading (data_name, seq_len):
     
@@ -99,53 +94,50 @@ def real_data_loading (data_name, seq_len):
     """  
     assert data_name in ['stock','energy', 'lung']
   
-    if data_name == 'stock':
+    if (data_name == 'stock') or (data_name == 'lung'):
         ori_data = np.loadtxt('data/stock_data.csv', delimiter = ",",skiprows = 1)
     elif data_name == 'energy':
         ori_data = np.loadtxt('data/energy_data.csv', delimiter = ",",skiprows = 1)
-    elif data_name == 'lung':
-        ori_data   = pickle.load(open("data/lung_cancer.p", "rb"))
+          
+    # Flip the data to make chronological data
+    ori_data = ori_data[::-1]
     
+    # Normalize the data
+    ori_data = MinMaxScaler(ori_data)
     
-    if data_name in ['stock','energy']:
+    # Preprocess the dataset
+    temp_data = []    
+    
+    # Cut data by sequence length
+    for i in range(0, len(ori_data) - seq_len):
         
-        # Flip the data to make chronological data
-        ori_data = ori_data[::-1]
-    
-        # Normalize the data
-        ori_data = MinMaxScaler(ori_data)
-    
-        # Preprocess the dataset
-        temp_data = []    
-    
-        # Cut data by sequence length
-        for i in range(0, len(ori_data) - seq_len):
+        _x = ori_data[i:i + seq_len]
+        temp_data.append(_x)
         
-            _x = ori_data[i:i + seq_len]
-            temp_data.append(_x)
-        
-        # Mix the datasets (to make it similar to i.i.d)
-        idx = np.random.permutation(len(temp_data))    
-        data = []
+    # Mix the datasets (to make it similar to i.i.d)
+    idx = np.random.permutation(len(temp_data))    
+    data = []
   
-        for i in range(len(temp_data)):
+    for i in range(len(temp_data)):
     
-            data.append(temp_data[idx[i]])
+        data.append(temp_data[idx[i]])
     
     # stock data 
     if data_name == 'stock':
         
-        X   = [np.hstack((0, data[k][:, 4])) for k in range(len(data))]
+        #X   = [np.hstack((0, data[k][:, 4])) for k in range(len(data))]
+        X   = [np.hstack((0, data[k][:, 0])) for k in range(len(data))]
     
     # energy data
     if data_name == 'energy':
-        X   = [np.hstack((0, data[k][:, 13])) for k in range(len(data))]
-        X   = [X[k] for k in range(1000)] 
+        X   = [np.hstack((0, data[k][:, 0])) for k in range(len(data))]
+        #X   = [np.hstack((0, data[k][:, 13])) for k in range(len(data))]
+        #X         = [X[k] for k in range(1000)] 
     
-    # lung data
     if data_name == 'lung':
+        
+        ori_data   = pickle.load(open("data/lung_cancer.p", "rb"))
         X          = [np.hstack((0, ori_data[k])) for k in range(len(ori_data))]
-        X          = [X[k] for k in range(2000)]
-    
-    
+        #X          = [X[k] for k in range(2000)]
+        
     return X
