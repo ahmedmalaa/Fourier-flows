b905759042f4975df6c2b4156c7e433f36d523ee
Evgeny Saveliev <e.s.saveliev@gmail.com>
2021-09-23 19:25:46 +0100

    Format code with black

--- fourier/transforms.py
+++ fourier/transforms.py
@@ -1,4 +1,3 @@
-
 # Copyright (c) 2021, Ahmed M. Alaa
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 
@@ -7,31 +6,31 @@
 This script contains functions for applying fourier transform and its inverse in a torch environment.
 Used as the "Fourier Transform" step in a Fourier Flow, can be preceded by other torch modules.
 
-""" 
+"""
 
 from __future__ import absolute_import, division, print_function
 
 import numpy as np
 import torch
-import torch.nn as nn 
+import torch.nn as nn
 
 from torch.distributions.multivariate_normal import MultivariateNormal
 
 import sys
 
 import warnings
+
 warnings.filterwarnings("ignore")
 
 if not sys.warnoptions:
     warnings.simplefilter("ignore")
-    
 
-# Helper functions    
-    
-    
+
+# Helper functions
+
+
 def flip(x, dim):
-    
-       
+
     """flipping helper
     
        Takes a vector as an input, then flips its elements from left to right
@@ -40,37 +39,38 @@ def flip(x, dim):
        :dim: splitting dimension
         
     """
-    
+
     xsize = x.size()
-    dim   = x.dim() + dim if dim < 0 else dim
-    x     = x.view(-1, *xsize[dim:])
-    x     = x.view(x.size(0), x.size(1), -1)[:, getattr(torch.arange(x.size(1)-1, 
-                      -1, -1), ('cpu','cuda')[x.is_cuda])().long(), :]
+    dim = x.dim() + dim if dim < 0 else dim
+    x = x.view(-1, *xsize[dim:])
+    x = x.view(x.size(0), x.size(1), -1)[
+        :, getattr(torch.arange(x.size(1) - 1, -1, -1), ("cpu", "cuda")[x.is_cuda])().long(), :
+    ]
     return x.view(xsize)
 
-    
+
 def reconstruct_DFT(x, component="real"):
-    
+
     """prepares input to the DFT inverse
     
        Takes a cropped frequency and creates a symmetric or anti-symmetric mirror of it before applying inverse DFT
         
     """
-    
-    if component=="real":
-        x_rec = torch.cat([x[0, :], flip(x[0, :], dim=0)[1:]], dim=0)
-    
-    elif component=="imag":
-        x_rec = torch.cat([x[1, :], -1 * flip(x[1, :], dim=0)[1:]], dim=0)
-    
-    return x_rec    
-    
 
-# Main DFT module    
-    
-    
+    if component == "real":
+        x_rec = torch.cat([x[0, :], flip(x[0, :], dim=0)[1:]], dim=0)
+
+    elif component == "imag":
+        x_rec = torch.cat([x[1, :], -1 * flip(x[1, :], dim=0)[1:]], dim=0)
+
+    return x_rec
+
+
+# Main DFT module
+
+
 class DFT(nn.Module):
-    
+
     """
     Discrete Fourier Transform (DFT) torch module
     
@@ -85,18 +85,18 @@ class DFT(nn.Module):
     :base_dist: base distribution of the flow, always defined as a multi-variate normal distribution
     
     """
-    
+
     def __init__(self, N_fft=100):
-        
+
         super(DFT, self).__init__()
-        
-        self.N_fft        = N_fft
-        self.crop_size    = int(self.N_fft/2) + 1
+
+        self.N_fft = N_fft
+        self.crop_size = int(self.N_fft / 2) + 1
         base_mu, base_cov = torch.zeros(self.crop_size * 2), torch.eye(self.crop_size * 2)
-        self.base_dist    = MultivariateNormal(base_mu, base_cov)
-        
-    def forward(self, x): 
-        
+        self.base_dist = MultivariateNormal(base_mu, base_cov)
+
+    def forward(self, x):
+
         """forward steps
         
         Step 1: Convert the input vector to numpy format
@@ -112,24 +112,30 @@ class DFT(nn.Module):
         Step 5: Compute the flow likelihood and Jacobean. Because DFT is a Vandermonde linear transform, Log-Jacob-Det = 0
         
         """
-        
-        if len(x.shape)==1:
-            
-            x             = x.reshape((1, -1)) 
 
-        x_numpy           = x.detach().float()
-        X_fft             = [np.fft.fftshift(np.fft.fft(x_numpy[k, :])) for k in range(x.shape[0])]
-        X_fft_train       = np.array([np.array([np.real(X_fft[k])[:self.crop_size] / self.N_fft,
-                                                np.imag(X_fft[k])[:self.crop_size] / self.N_fft]) for k in range(len(X_fft))])
-        x_fft             = torch.from_numpy(X_fft_train).float()   
+        if len(x.shape) == 1:
 
-        log_pz            = self.base_dist.log_prob(x_fft.view(-1, x_fft.shape[1] * x_fft.shape[2]))
-        log_jacob         = 0
-        
-        return x_fft, log_pz, log_jacob 
-    
+            x = x.reshape((1, -1))
+
+        x_numpy = x.detach().float()
+        X_fft = [np.fft.fftshift(np.fft.fft(x_numpy[k, :])) for k in range(x.shape[0])]
+        X_fft_train = np.array(
+            [
+                np.array(
+                    [np.real(X_fft[k])[: self.crop_size] / self.N_fft, np.imag(X_fft[k])[: self.crop_size] / self.N_fft]
+                )
+                for k in range(len(X_fft))
+            ]
+        )
+        x_fft = torch.from_numpy(X_fft_train).float()
+
+        log_pz = self.base_dist.log_prob(x_fft.view(-1, x_fft.shape[1] * x_fft.shape[2]))
+        log_jacob = 0
+
+        return x_fft, log_pz, log_jacob
+
     def inverse(self, x):
-        
+
         """Inverse steps
         
         Step 1: Convert the input vector to numpy format with size NUM_SAMPLES x 2 x N_fft
@@ -147,13 +153,19 @@ class DFT(nn.Module):
         
         """
 
-        x_numpy           = x.view((-1, 2, self.crop_size))
-        
-        x_numpy_r         = [reconstruct_DFT(x_numpy[u, :, :], component="real").detach().numpy() for u in range(x_numpy.shape[0])]
-        x_numpy_i         = [reconstruct_DFT(x_numpy[u, :, :], component="imag").detach().numpy() for u in range(x_numpy.shape[0])]
-        
-        x_ifft            = [self.N_fft * np.real(np.fft.ifft(np.fft.ifftshift(x_numpy_r[u] + 1j * x_numpy_i[u]))) for u in range(x_numpy.shape[0])]
-        x_ifft_out        = torch.from_numpy(np.array(x_ifft)).float()          
-        
-        return x_ifft_out        
+        x_numpy = x.view((-1, 2, self.crop_size))
 
+        x_numpy_r = [
+            reconstruct_DFT(x_numpy[u, :, :], component="real").detach().numpy() for u in range(x_numpy.shape[0])
+        ]
+        x_numpy_i = [
+            reconstruct_DFT(x_numpy[u, :, :], component="imag").detach().numpy() for u in range(x_numpy.shape[0])
+        ]
+
+        x_ifft = [
+            self.N_fft * np.real(np.fft.ifft(np.fft.ifftshift(x_numpy_r[u] + 1j * x_numpy_i[u])))
+            for u in range(x_numpy.shape[0])
+        ]
+        x_ifft_out = torch.from_numpy(np.array(x_ifft)).float()
+
+        return x_ifft_out
--- metrics/MAE.py
+++ metrics/MAE.py
@@ -1,5 +1,3 @@
-
-
 # Copyright (c) 2021, Ahmed M. Alaa
 # Licensed under the BSD 3-clause license (see LICENSE.txt)
 
@@ -9,25 +7,25 @@ from models.sequential import *
 
 
 def train_RNN(X_synth):
-    
-    predictive_model     = RNNmodel(mode="LSTM", HIDDEN_UNITS=100, NUM_LAYERS=2, MAX_STEPS=100, INPUT_SIZE=1)
 
-    X_                   = [X_synth[k][:len(X_synth[k])-1] for k in range(len(X_synth))]
-    Y_                   = [X_synth[k][1:] for k in range(len(X_synth))]
-    
+    predictive_model = RNNmodel(mode="LSTM", HIDDEN_UNITS=100, NUM_LAYERS=2, MAX_STEPS=100, INPUT_SIZE=1)
+
+    X_ = [X_synth[k][: len(X_synth[k]) - 1] for k in range(len(X_synth))]
+    Y_ = [X_synth[k][1:] for k in range(len(X_synth))]
+
     predictive_model.fit(X_, Y_, verbosity=False)
-    
+
     return predictive_model
 
 
 def computeMAE(X_real, X_synth):
 
-	X_true                  = [X_real[k][:len(X_real[k])-1] for k in range(len(X_real))]
-	Y_true                  = [X_real[k][1:] for k in range(len(X_real))]
+    X_true = [X_real[k][: len(X_real[k]) - 1] for k in range(len(X_real))]
+    Y_true = [X_real[k][1:] for k in range(len(X_real))]
 
-	predictive_model        = train_RNN([X_synth[k] for k in range(len(X_synth))])    
+    predictive_model = train_RNN([X_synth[k] for k in range(len(X_synth))])
 
-	X_pred                  = predictive_model.predict(X_true)
-	MAEscore                = np.mean(np.abs(np.array(X_pred) - np.array(Y_true)))
+    X_pred = predictive_model.predict(X_true)
+    MAEscore = np.mean(np.abs(np.array(X_pred) - np.array(Y_true)))
 
-	return MAEscore
+    return MAEscore
